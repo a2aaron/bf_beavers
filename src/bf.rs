18c7bada63c1ca42ced9772a8b67a84c787611ed
Aaron Kofsky <aaronko@umich.edu>
2022-05-03 16:59:26 -0400

    Start work on infinite loop detection.

--- src/bf.rs
+++ src/bf.rs
@@ -9,7 +9,7 @@ const EXTEND_MEMORY_AMOUNT: usize = 1;
 #[derive(Debug)]
 pub struct ExecutionContext {
     memory_behavior: MemoryBehavior,
-    memory: Vec<i8>,
+    memory: Vec<u8>,
     memory_pointer: usize,
     program: Program,
     program_pointer: usize,
@@ -26,72 +26,88 @@ impl ExecutionContext {
         }
     }
 
-    pub fn step(&mut self) {
-        debug_assert!(!self.halted());
+    pub fn step(&mut self) -> (usize, ExecutionState) {
         use self::MemoryBehavior::*;
         use Instr::*;
 
         let instruction = self.program.get(self.program_pointer);
+
         match instruction {
-            Plus => {
-                self.memory[self.memory_pointer] = self.memory[self.memory_pointer].wrapping_add(1)
-            }
-            Minus => {
-                self.memory[self.memory_pointer] = self.memory[self.memory_pointer].wrapping_sub(1)
-            }
-            Left => match self.memory_behavior {
-                Wrapping(modulo) => {
-                    self.memory_pointer = wrapping_add(self.memory_pointer, -1, modulo)
+            None => (0, ExecutionState::Halted),
+            Some(instruction) => match instruction {
+                ExtendedInstr::BaseInstr(instruction) => {
+                    match instruction {
+                        Plus => {
+                            self.memory[self.memory_pointer] =
+                                self.memory[self.memory_pointer].wrapping_add(1)
+                        }
+                        Minus => {
+                            self.memory[self.memory_pointer] =
+                                self.memory[self.memory_pointer].wrapping_sub(1)
+                        }
+                        Left => match self.memory_behavior {
+                            Wrapping(modulo) => {
+                                self.memory_pointer = wrapping_add(self.memory_pointer, -1, modulo)
+                            }
+                            InfiniteRightwards => {
+                                self.memory_pointer = self.memory_pointer.saturating_sub(1)
+                            }
+                        },
+                        Right => match self.memory_behavior {
+                            Wrapping(modulo) => {
+                                self.memory_pointer = wrapping_add(self.memory_pointer, 1, modulo)
+                            }
+                            InfiniteRightwards => {
+                                self.memory_pointer += 1;
+                                if self.memory_pointer >= self.memory.len() {
+                                    self.memory.extend([0; EXTEND_MEMORY_AMOUNT].iter());
+                                }
+                            }
+                        },
+                        StartLoop => {
+                            if self.memory[self.memory_pointer] == 0 {
+                                self.program_pointer = self
+                                    .program
+                                    .matching_loop(self.program_pointer)
+                                    .expect("missing StartLoop dict entry!")
+                                    + 1;
+                            }
+                        }
+                        EndLoop => {
+                            if self.memory[self.memory_pointer] != 0 {
+                                self.program_pointer = self
+                                    .program
+                                    .matching_loop(self.program_pointer)
+                                    .expect("missing EndLoop dict entry!");
+                            }
+                        }
+                    }
+                    self.program_pointer += 1;
+                    (1, ExecutionState::Running)
                 }
-                InfiniteRightwards => self.memory_pointer = self.memory_pointer.saturating_sub(1),
-            },
-            Right => match self.memory_behavior {
-                Wrapping(modulo) => {
-                    self.memory_pointer = wrapping_add(self.memory_pointer, 1, modulo)
-                }
-                InfiniteRightwards => {
-                    self.memory_pointer += 1;
-                    if self.memory_pointer >= self.memory.len() {
-                        self.memory.extend([0; EXTEND_MEMORY_AMOUNT].iter());
+                ExtendedInstr::LoopIfNonzero => {
+                    if self.memory[self.memory_pointer] == 0 {
+                        self.program_pointer += 1;
+                        (2, ExecutionState::Running)
+                    } else {
+                        (2, ExecutionState::InfiniteLoop)
                     }
                 }
             },
-            StartLoop => {
-                if self.memory[self.memory_pointer] == 0 {
-                    self.program_pointer = self
-                        .program
-                        .matching_loop(self.program_pointer)
-                        .expect("missing StartLoop dict entry!")
-                        + 1;
-                }
-            }
-            EndLoop => {
-                if self.memory[self.memory_pointer] != 0 {
-                    self.program_pointer = self
-                        .program
-                        .matching_loop(self.program_pointer)
-                        .expect("missing EndLoop dict entry!");
-                }
-            }
         }
-        self.program_pointer += 1;
-    }
-
-    pub fn halted(&self) -> bool {
-        self.program_pointer >= self.program.instrs.len()
     }
 
     pub fn print_state(&self) {
-        let this_instr = if self.halted() {
-            "HALTED".to_string()
+        let this_instr = if let Some(instr) = self.program.get(self.program_pointer) {
+            instr.to_string()
         } else {
-            self.program.get(self.program_pointer).to_string()
+            "HALTED".to_string()
         };
 
         let memory: String = self
             .memory
             .iter()
-            .map(|x| format!("{: >4}", x))
+            .map(|x| format!("{:0>2X}", x))
             .intersperse(" ".to_string())
             .collect();
         println!("[{}] (this_instr = {})", memory, this_instr,);
@@ -102,9 +118,9 @@ impl ExecutionContext {
             .enumerate()
             .map(|(index, _)| {
                 if index == self.memory_pointer {
-                    "^^^^"
+                    "^^"
                 } else {
-                    "    "
+                    "  "
                 }
             })
             .intersperse(" ")
@@ -128,23 +144,33 @@ pub enum MemoryBehavior {
     InfiniteRightwards,
 }
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum ExecutionState {
+    Running,
+    Halted,
+    InfiniteLoop,
+}
+
 #[derive(Debug, Clone)]
 pub struct Program {
-    pub instrs: Vec<Instr>,
+    original_instrs: Vec<Instr>,
+    extended_instrs: Vec<ExtendedInstr>,
     loop_dict: HashMap<usize, usize>,
 }
 
 impl Program {
     pub fn new(instrs: &[Instr]) -> Result<Program, CompileError> {
-        let loop_dict = loop_dict(instrs)?;
+        let extended_instrs = ExtendedInstr::new(instrs);
+        let loop_dict = loop_dict(&extended_instrs)?;
         Ok(Program {
-            instrs: instrs.to_vec(),
+            original_instrs: instrs.to_vec(),
+            extended_instrs,
             loop_dict,
         })
     }
 
-    pub fn get(&self, i: usize) -> Instr {
-        self.instrs[i]
+    fn get(&self, i: usize) -> Option<ExtendedInstr> {
+        self.extended_instrs.get(i).cloned()
     }
 
     fn matching_loop(&self, i: usize) -> Option<usize> {
@@ -154,7 +180,7 @@ impl Program {
 
 impl Display for Program {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", to_string(&self.instrs))
+        write!(f, "{}", to_string(&self.original_instrs))
     }
 }
 
@@ -170,25 +196,66 @@ impl TryFrom<&str> for Program {
     }
 }
 
-fn loop_dict(program: &[Instr]) -> Result<HashMap<usize, usize>, CompileError> {
+#[derive(Debug, Copy, Clone)]
+enum ExtendedInstr {
+    BaseInstr(Instr),
+    LoopIfNonzero,
+}
+
+impl ExtendedInstr {
+    fn new(program: &[Instr]) -> Vec<ExtendedInstr> {
+        let mut extended_instrs = vec![];
+        let mut i = 0;
+        while i < program.len() {
+            let this_instr = program[i];
+            let next_instr = program.get(i + 1);
+            let extended_instr = match (this_instr, next_instr) {
+                (Instr::StartLoop, Some(Instr::EndLoop)) => {
+                    i += 2;
+                    ExtendedInstr::LoopIfNonzero
+                }
+                (instr, _) => {
+                    i += 1;
+                    ExtendedInstr::BaseInstr(instr)
+                }
+            };
+            extended_instrs.push(extended_instr);
+        }
+        extended_instrs
+    }
+}
+
+impl Display for ExtendedInstr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ExtendedInstr::BaseInstr(instr) => write!(f, "{}", instr),
+            ExtendedInstr::LoopIfNonzero => write!(f, "[]"),
+        }
+    }
+}
+
+fn loop_dict(program: &[ExtendedInstr]) -> Result<HashMap<usize, usize>, CompileError> {
     use Instr::*;
     let mut hashmap = HashMap::new();
     let mut startloop_locs = Vec::new();
     for (i, &instr) in program.iter().enumerate() {
         match instr {
-            Plus | Minus | Left | Right => (),
-            StartLoop => {
-                startloop_locs.push(i);
-            }
-            EndLoop => {
-                match startloop_locs.pop() {
-                    Some(start_loop) => {
-                        hashmap.insert(i, start_loop);
-                        hashmap.insert(start_loop, i);
-                    }
-                    None => return Err(CompileError::UnmatchedEndLoop { index: i }),
-                };
-            }
+            ExtendedInstr::LoopIfNonzero => (),
+            ExtendedInstr::BaseInstr(instr) => match instr {
+                Plus | Minus | Left | Right => (),
+                StartLoop => {
+                    startloop_locs.push(i);
+                }
+                EndLoop => {
+                    match startloop_locs.pop() {
+                        Some(start_loop) => {
+                            hashmap.insert(i, start_loop);
+                            hashmap.insert(start_loop, i);
+                        }
+                        None => return Err(CompileError::UnmatchedEndLoop { index: i }),
+                    };
+                }
+            },
         }
     }
     if !startloop_locs.is_empty() {
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,29 @@
 
 use std::convert::TryFrom;
 
+use crate::bf::ExecutionState;
+
 pub mod bf;
 pub mod generate;
 
 fn step_count(program: &bf::Program, max_steps: usize) -> Option<usize> {
     let mut ctx = bf::ExecutionContext::new(program);
-    for steps in 1..max_steps {
-        ctx.step();
-        if ctx.halted() {
-            return Some(steps);
+    let mut total_real_steps = 0;
+    for _ in 1..max_steps {
+        let (real_steps, state) = ctx.step();
+        total_real_steps += real_steps;
+        match state {
+            ExecutionState::Halted => {
+                return Some(total_real_steps);
+            }
+            ExecutionState::InfiniteLoop => {
+                // eprintln!("bailed: {} (infinite loop)", program);
+                return None;
+            }
+            ExecutionState::Running => (),
         }
     }
-    eprintln!("bailed: {}", program);
+    eprintln!("bailed: {} (timeout)", program);
     None
 }
 
@@ -41,29 +52,41 @@ fn trace(program: &bf::Program) {
     let mut ctx = bf::ExecutionContext::new(program);
     ctx.print_state();
     loop {
-        ctx.step();
+        let (_, state) = ctx.step();
         ctx.print_state();
-        if ctx.halted() {
-            break;
+        match state {
+            ExecutionState::Running => (),
+            ExecutionState::Halted => {
+                println!("Halted.");
+                break;
+            }
+            ExecutionState::InfiniteLoop => {
+                println!("Infinite loop detected.");
+                break;
+            }
         }
     }
 }
 
 fn main() {
-    // let program = bf::Program::try_from("+++---").unwrap();
-    // trace(&program);
-    // println!("{:?}", program);
-    // println!("{:?}", step_count(&program, 1_000_000));
     let max_steps = 1_000_000;
-    for i in 0..10 {
-        let (programs, steps) = beaver(i, max_steps);
+    let debug = false;
+    if debug {
+        let program = bf::Program::try_from("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.").unwrap();
+        println!("{:?}", program);
+        trace(&program);
+        println!("{:?}", step_count(&program, max_steps));
+    } else {
+        for i in 0..6 {
+            let (programs, steps) = beaver(i, max_steps);
 
-        println!(
+            println!(
             "Best Programs for Beaver (length = {}, steps = {} or best runs for longer than {})",
             i, steps, max_steps
         );
-        for program in programs {
-            println!("{}", program);
+            for program in programs {
+                println!("{}", program);
+            }
         }
     }
 }
