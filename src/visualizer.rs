3673e8b4231bd3530fcd9092e0ac54b929c7afca
Aaron Kofsky <aaronko@umich.edu>
2022-06-13 11:20:05 -0400

    Move cached history data to own struct.

--- src/visualizer.rs
+++ src/visualizer.rs
@@ -14,8 +14,25 @@ use crossterm::{
 
 use crate::bf::{ExecutionContext, ExecutionStatus, Program};
 
+#[derive(Debug, Clone)]
+struct HistoryData {
+    real_steps: usize,
+    status: ExecutionStatus,
+    exec_ctx: ExecutionContext,
+}
+
+impl HistoryData {
+    fn new(real_steps: usize, status: ExecutionStatus, exec_ctx: &ExecutionContext) -> HistoryData {
+        HistoryData {
+            real_steps,
+            status,
+            exec_ctx: exec_ctx.clone(),
+        }
+    }
+}
+
 struct History {
-    history: HashMap<usize, [((usize, ExecutionStatus), ExecutionContext); 1024]>,
+    history: HashMap<usize, [HistoryData; 1024]>,
     program: Program,
 }
 
@@ -27,20 +44,27 @@ impl History {
         }
     }
 
-    fn get(&mut self, step: usize) -> ((usize, ExecutionStatus), ExecutionContext) {
+    fn get(&mut self, step: usize) -> HistoryData {
         let nearest_gradiation = step / 1024;
         let entry = match self.history.entry(nearest_gradiation) {
             hash_map::Entry::Occupied(entry) => entry,
             hash_map::Entry::Vacant(entry) => {
+                let mut real_steps = 0;
                 let mut exec_ctx = ExecutionContext::new(&self.program);
                 for _ in 0..nearest_gradiation * 1024 {
-                    exec_ctx.step();
+                    let (real_steps_delta, _) = exec_ctx.step();
+                    real_steps += real_steps_delta;
                 }
                 let mut array = Vec::with_capacity(1024);
-                array.push(((1, ExecutionStatus::Running), exec_ctx.clone()));
+                array.push(HistoryData::new(
+                    real_steps,
+                    ExecutionStatus::Running,
+                    &exec_ctx,
+                ));
                 for _ in 0..1023 {
-                    let state = exec_ctx.step();
-                    array.push((state, exec_ctx.clone()))
+                    let (real_steps_delta, status) = exec_ctx.step();
+                    real_steps += real_steps_delta;
+                    array.push(HistoryData::new(real_steps, status, &exec_ctx));
                 }
 
                 entry.insert_entry(array.try_into().unwrap())
@@ -52,21 +76,28 @@ impl History {
 
 pub fn run(program: &Program, starting_step: usize) {
     fn print_state(
-        ((_, state), exe_ctx): &((usize, ExecutionStatus), ExecutionContext),
+        HistoryData {
+            real_steps,
+            status,
+            exec_ctx,
+        }: &HistoryData,
         curr_step: usize,
     ) {
         crossterm::execute! { stdout(), cursor::MoveTo(0,0) }.unwrap();
         crossterm::execute! { stdout(), Clear(ClearType::All) }.unwrap();
 
-        let displayed_state = crossterm::style::style(format!("{:?}", state));
-        let displayed_state = match state {
-            ExecutionStatus::Running => displayed_state,
-            ExecutionStatus::Halted => displayed_state.on_red(),
-            ExecutionStatus::InfiniteLoop(_) => displayed_state.on_cyan(),
+        let displayed_status = crossterm::style::style(format!("{:?}", status));
+        let displayed_status = match status {
+            ExecutionStatus::Running => displayed_status,
+            ExecutionStatus::Halted => displayed_status.on_red(),
+            ExecutionStatus::InfiniteLoop(_) => displayed_status.on_cyan(),
         };
-        println!("Steps: {}, State: {}", curr_step, displayed_state);
+        println!(
+            "Steps: {} (Actual: {}), Status: {}",
+            curr_step, real_steps, displayed_status
+        );
 
-        exe_ctx.print_state(true);
+        exec_ctx.print_state(true);
     }
     let mut history = History::new(program);
     let mut curr_step = starting_step;
@@ -81,9 +112,9 @@ pub fn run(program: &Program, starting_step: usize) {
 
         if let Event::Key(event) = event {
             // If shift is held, jump to the end/start of this loop.
-            let curr_exec = &history.get(curr_step).1;
+            let exec_ctx = &history.get(curr_step).exec_ctx;
             let corresponding_loop = if event.modifiers.contains(KeyModifiers::SHIFT) {
-                curr_exec.current_loop_bounds()
+                exec_ctx.current_loop_bounds()
             } else {
                 None
             };
@@ -100,8 +131,8 @@ pub fn run(program: &Program, starting_step: usize) {
                     _ => (),
                 }
 
-                let curr_exec = &history.get(curr_step).1;
-                if let Some((start, end)) = corresponding_loop && (start..end).contains(&curr_exec.program_pointer()) {
+                let exec_ctx = &history.get(curr_step).exec_ctx;
+                if let Some((start, end)) = corresponding_loop && (start..end).contains(&exec_ctx.program_pointer()) {
                     continue;
                 } else {
                      break;
